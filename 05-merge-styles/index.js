a8c39512aeb3e594c970146f522263cb9eb34efa
Anasstassia <gubinaa75@gmail.com>
2021-11-05 00:21:10 +0300

    feat: task is done

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+const { createReadStream, createWriteStream } = require("fs");
+
+const filesFolder = path.join(__dirname, "/styles");
+const bundleFilePath = path.join(__dirname, "/project-dist/bundle.css");
+const newBundle = createWriteStream(bundleFilePath);
+
+newBundle.once("open", () =>
+  fs.readdir(filesFolder, { withFileTypes: true }, (err, files) => {
+    for (let file of files) {
+      const extname = path.extname(file.name);
+      const extn = extname.slice(1);
+      if (file.isFile() && extn === "css") {
+        const stream = createReadStream(
+          path.join(filesFolder, path.basename(file.name))
+        );
+        stream.on("data", (chunk) => {
+          str.write(chunk);
+        });
+      }
+    }
+  })
+);
